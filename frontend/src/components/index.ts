f5c809a66b0f5c90fb978fe728b808abaa3d4948
Rg0rajesh <rajeshchowdary594@gmail.com>
2026-08-13 10:00:00 +0530

    chore(frontend): add components barrel export

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,1 +1,7 @@
-﻿
+// frontend/src/components/index.ts
+export { Sidebar } from "./Sidebar";
+export { Navbar } from "./Navbar";
+export { Logo } from "./Logo";
+export { LogStream } from "./LogStream";
+export { AgentPanel } from "./AgentPanel";
+export { StatusPill } from "./StatusPill";
